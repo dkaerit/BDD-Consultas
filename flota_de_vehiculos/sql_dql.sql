c73ef1c4d78cd027f9fcaf9aa02c9f03e5aa4c78
Diego Vázquez Campos <kaerit@hotmail.com>
2023-07-28 17:15:44 +0100

    Update and rename sql_dql.md to sql_dql.sql

--- flota_de_vehiculos/sql_dql.sql
+++ flota_de_vehiculos/sql_dql.sql
@@ -1,5 +1,4 @@
-a) Vehículos que solo han repostado en estaciones de la cadena DISA:
-```sql
+-- a) Vehículos que solo han repostado en estaciones de la cadena DISA:
 -- Selecciona los vehículos que solo han repostado en estaciones de la cadena DISA
 SELECT DISTINCT V.M
 FROM VEHICULO V
@@ -11,10 +10,8 @@ WHERE NOT EXISTS (
     WHERE V.M = C.M
     AND E.CADENA <> 'DISA'
 );
-```
 
-b) Precio medio del litro de gasolina en cada una de las estaciones de CEPSA durante el mes de enero de 2023:
-```sql
+-- b) Precio medio del litro de gasolina en cada una de las estaciones de CEPSA durante el mes de enero de 2023:
 -- Calcula el precio medio del litro de gasolina en cada estación de CEPSA durante enero de 2023
 SELECT E.E, AVG(P)
 FROM ESTACION E
@@ -24,10 +21,8 @@ WHERE E.CADENA = 'CEPSA'
 AND MONTH(C.F) = 1
 AND YEAR(C.F) = 2023
 GROUP BY E.E;
-```
 
-c) Estaciones que han vendido más de 5000 litros de gasolina en un mismo día:
-```sql
+-- Estaciones que han vendido más de 5000 litros de gasolina en un mismo día:
 -- Selecciona las estaciones que han vendido más de 5000 litros de gasolina en un mismo día
 SELECT E.E
 FROM ESTACION E
@@ -35,10 +30,8 @@ JOIN COMBUSTIBLE C ON E.E = C.E
 WHERE C.C = 'gasolina'
 GROUP BY E.E, C.F
 HAVING SUM(C.L) > 5000;
-```
 
-d) Estación que el día 1 de junio de 2023 vendía la gasolina más barata:
-```sql
+-- d) Estación que el día 1 de junio de 2023 vendía la gasolina más barata:
 -- Selecciona la estación que vendía la gasolina más barata el día 1 de junio de 2023
 SELECT E.E
 FROM ESTACION E
@@ -52,14 +45,11 @@ AND C.P = (
     WHERE C = 'gasolina'
     AND F = '2023-06-01'
 );
-```
 
-e) Vehículos tales que al menos el 30% de sus repostajes los han hecho en una misma estación:
-```sql
+--e) Vehículos tales que al menos el 30% de sus repostajes los han hecho en una misma estación:
 -- Selecciona los vehículos que han realizado al menos el 30% de sus repostajes en una misma estación
 SELECT V.M
 FROM VEHICULO V
 JOIN COMBUSTIBLE C ON V.M = C.M
 GROUP BY V.M
 HAVING SUM(CASE WHEN C.E = V.E THEN 1 ELSE 0 END) >= 0.3 * COUNT(*) ;
-```
